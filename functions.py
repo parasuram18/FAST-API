7792c6bb459db4f0f509f574c104ea722ce39fd7
parasuram <parasuramech@gmail.com>
2025-07-16 15:20:33 +0530

    [MOD] dbname change

--- functions.py
+++ functions.py
@@ -8,4 +8,13 @@ try:
     isvalid = ph.verify(hash, "mysecret")
 except:
     isvalid = False
-print(isvalid)
+
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=['argon2'], deprecated="auto")
+
+password = "$argon2id$v=19$m=65536,t=3,p=4$CWcMeoDuZlQIzOW8Jf5bQg$UwCB+YsV9A+9aRSsObKejhJaavt28Xy12ig2lzvcg8M" #pwd_context.hash('admin@123')
+
+is_valid = pwd_context.verify('parasu@123', password)
+
+print(is_valid)
